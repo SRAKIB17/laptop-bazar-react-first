f11af8788f6a93225dad66b81c7835c656bf9478
SRAKIB17 <rakibulssc5@gmail.com>
2022-03-26 02:32:12 +0600

    error fixed

--- src/Component/ShowCart/ShowCart.js
+++ src/Component/ShowCart/ShowCart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './ShowCart.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
+
 const ShowCart = (props) => {
     const {name , image} = props.cart
     const delItem = props.newCart
@@ -15,7 +14,7 @@ const ShowCart = (props) => {
                     <td><img src={image} alt=""  className='cart-img' /></td>
                     <td>{name.substr(0, 30)}..</td>
                     <td>
-                        <button style={{border: 'none'}}>
+                        <button style={{border: 'none'}} onClick={props.FontAwesomeIcon}>
                             <i style={{fontSize: '20px',color: 'red'}} class="fa">&#xf014;</i>
                         </button>
                     </td>
